0cb3c0e1fc0f90417feb1622d3b6e273e23fa4f9
alexolivaresdv <hp.alex.ao@gmail.com>
2017-10-31 21:36:38 -0300

    Calcular area de un rectangulo.

--- src/evajava1/Rectangulo.java
+++ src/evajava1/Rectangulo.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package evajava1;
-
-/**
- *
+import java.util.Scanner;//paso 1
+/**************************************************************************
+ * Alex Olivares Del Valle 
  * @author LAB17
+ * Fecha: Octubre 31 del 2017
+ * Programa: Corrección Prueba1
+ * Responsabilidad: Calcular el area de un rectangulo
  */
-public class Rectangulo {
-    
+public class Rectangulo 
+{
+   public static void main(String[] args)
+   {
+        float area, base, altura;//paso2
+        Scanner sc = new Scanner(System.in);//paso 3
+        
+        //paso 4
+        System.out.println("Ingrese la base del rectangulo");
+        base = sc.nextFloat();// Leo y guardo los datos de entrada 
+        System.out.println("Ingrese la altura del rectangulo");
+        altura = sc.nextFloat();// Leo y guardo los datos de entrada 
+        
+        //paso 5
+        area=base*altura;
+        System.out.println("El area de su rectangulo es "+area+"mts2");
+        
+   } 
 }
